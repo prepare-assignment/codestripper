de6d55ce98dfbe58c1a8998f925db242543ccfc0
Martijn Bonajo <m.bonajo@fontys.nl>
2023-11-01 21:22:37 +0100

    feat: poetry + allow_outside + tests

---
+++ prepare_codestripper/main.py
@@ -0,0 +1,35 @@
+from typing import List, Optional
+
+from codestripper.utils import set_logger_level  # type: ignore
+from codestripper.code_stripper import strip_files  # type: ignore
+from prepare_toolbox.core import get_input, set_output, set_failed
+from prepare_toolbox.file import get_matching_files
+
+
+def strip() -> None:
+    try:
+        include: List[str] = get_input("include", required=True)
+        exclude: Optional[List[str]] = get_input("exclude")
+        cwd: str = get_input("working-directory")
+        allow_outside: bool = get_input("allow-outside-working-directory")
+        out: str = get_input("output-directory")
+        comment: str = get_input("comment")
+        recursive: bool = get_input("recursive")
+        verbosity: int = get_input("verbosity")
+        dry_run: bool = get_input("dry-run")
+
+        files = get_matching_files(include, exclude, allow_outside_working_dir=allow_outside,
+                                   relative_to=cwd, recursive=recursive)
+
+        if files is None or len(files) == 0:
+            set_failed(f"No files matched")
+        set_logger_level("prepare_codestripper", verbosity)
+        stripped = strip_files(files, working_directory=cwd, comment=comment, output=out, dry_run=dry_run)
+        set_output("matched-files", files)
+        set_output("stripped-files", stripped)
+    except Exception as e:
+        set_failed(e)
+
+
+if __name__ == "__main__":
+    strip()
---
+++ tests/test_main.py
@@ -0,0 +1,106 @@
+from typing import Any
+
+import pytest
+import pytest_mock
+
+from prepare_codestripper.main import strip
+
+
+def test_inputs(mocker: pytest_mock.MockerFixture) -> None:
+    """
+    This basically only tests that all params are passed correctly.
+    The main work is done by the codestripper itself
+
+    :param mocker: mocker
+    :return:
+    """
+    include = ["a.txt", "b.txt"]
+    exclude = ["c.txt"]
+    working_directory = "solution"
+    allow_outside = False
+    output_directory = "out"
+    recursive = True
+    verbosity = 3
+    dry_run = False
+    comment = "//"
+    matched_files = ["a.txt"]
+
+    def __get_input(key: str, required: bool = False) -> Any:
+        if key == "include":
+            return include
+        elif key == "exclude":
+            return exclude
+        elif key == "working-directory":
+            return working_directory
+        elif key == "allow-outside-working-directory":
+            return allow_outside
+        elif key == "output-directory":
+            return output_directory
+        elif key == "recursive":
+            return recursive
+        elif key == "verbosity":
+            return verbosity
+        elif key == "dry-run":
+            return dry_run
+        elif key == "comment":
+            return comment
+
+    mocker.patch("prepare_codestripper.main.get_input", side_effect=__get_input)
+    mocked_matching_files = mocker.patch("prepare_codestripper.main.get_matching_files", return_value=matched_files)
+    mocked_strip_files = mocker.patch("prepare_codestripper.main.strip_files")
+    mocked_set_output = mocker.patch("prepare_codestripper.main.set_output")
+
+    strip()
+
+    mocked_matching_files.assert_called_once_with(include, exclude, allow_outside_working_dir=allow_outside,
+                                                  relative_to=working_directory, recursive=recursive)
+    mocked_strip_files.assert_called_once_with(matched_files, working_directory=working_directory,
+                                               comment=comment, output=output_directory, dry_run=dry_run)
+    assert mocked_set_output.call_count == 2
+
+
+def test_no_matches(mocker: pytest_mock.MockerFixture) -> None:
+    """
+    Test that it fails if no files match
+
+    :param mocker: mocker
+    :return:
+    """
+    include = ["a.txt", "b.txt"]
+    exclude = ["c.txt"]
+    working_directory = "solution"
+    allow_outside = False
+    output_directory = "out"
+    recursive = True
+    verbosity = 3
+    dry_run = False
+    comment = "//"
+    matched_files = []
+
+    def __get_input(key: str, required: bool = False) -> Any:
+        if key == "include":
+            return include
+        elif key == "exclude":
+            return exclude
+        elif key == "working-directory":
+            return working_directory
+        elif key == "allow-outside-working-directory":
+            return allow_outside
+        elif key == "output-directory":
+            return output_directory
+        elif key == "recursive":
+            return recursive
+        elif key == "verbosity":
+            return verbosity
+        elif key == "dry-run":
+            return dry_run
+        elif key == "comment":
+            return comment
+
+    mocker.patch("prepare_codestripper.main.get_input", side_effect=__get_input)
+    mocker.patch("prepare_codestripper.main.get_matching_files", return_value=matched_files)
+    mocker.patch("prepare_codestripper.main.strip_files")
+    mocker.patch("prepare_codestripper.main.set_output")
+
+    with pytest.raises(SystemExit):
+        strip()
