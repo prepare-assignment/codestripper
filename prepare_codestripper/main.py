b4d8afb174ec6ee414894b2f6d845678b834e80f
Bonajo <m.bonajo@fontys.nl>
2024-02-01 13:02:35 +0100

    feat: add debugging statements

--- prepare_codestripper/main.py
+++ prepare_codestripper/main.py
@@ -2,14 +2,16 @@ from typing import List, Optional
 
 from codestripper.utils import set_logger_level  # type: ignore
 from codestripper.code_stripper import strip_files  # type: ignore
-from prepare_toolbox.core import get_input, set_output, set_failed
+from prepare_toolbox.core import get_input, set_output, set_failed, debug, info
 from prepare_toolbox.file import get_matching_files
 
 
 def strip() -> None:
     try:
         include: List[str] = get_input("include", required=True)
+        debug(f"include: {include}")
         exclude: Optional[List[str]] = get_input("exclude")
+        debug(f"exclude:  {exclude}")
         cwd: str = get_input("working-directory")
         allow_outside: bool = get_input("allow-outside-working-directory")
         out: str = get_input("output-directory")
@@ -23,6 +25,7 @@ def strip() -> None:
 
         if files is None or len(files) == 0:
             set_failed(f"No files matched")
+        info(f"Matched files: {files}")
         set_logger_level("prepare_codestripper", verbosity)
         stripped = strip_files(files, working_directory=cwd, comment=comment, output=out, dry_run=dry_run)
         set_output("matched-files", files)
